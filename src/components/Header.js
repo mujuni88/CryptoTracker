ebfc6e70fc84676b799859b58bcd7a030af5bddf
Joe Buza <joebm08@gmail.com>
2017-12-21 17:47:43 -0600

    feat: init app setup

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Header = () => (  
+  <View>
+    <Text>Cryptocurrency Tracker</Text>
+  </View>
+)
+
+export default Header
